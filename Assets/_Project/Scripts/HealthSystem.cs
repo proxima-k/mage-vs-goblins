13a6e818db58ddccc04636c0598c7c08ada5bec3
proxima-k <leonnkent@gmail.com>
2022-10-03 21:09:39 +0800

    Setup basic health system

---
+++ Assets/_Project/Scripts/HealthSystem.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class HealthSystem {
+    public int Health => _health;
+    public int MaxHealth => _maxHealth;
+    
+    private int _health;
+    private int _maxHealth;
+
+    public event Action OnDeath;
+    
+    public HealthSystem(int health) {
+        _health = health;
+        _maxHealth = health;
+    }
+
+    public void Heal(int amount) {
+        _health += Mathf.Abs(amount);
+        if (_health > _maxHealth) _health = _maxHealth;
+    }
+
+    public void Damage(int amount) {
+        _health -= Mathf.Abs(amount);
+        if (_health <= 0) {
+            _health = 0;
+            OnDeath?.Invoke();
+        }
+    }
+    
+    // public void UpgradeMaxHealth(int maxHealth) {
+    //     _maxHealth = maxHealth;
+    // }
+}
