11959a690f86fe6e74074da3a5bda9a2a58ee376
proxima-k <leonnkent@gmail.com>
2022-10-04 12:57:00 +0800

    Setup base ability script to implement more unique abilities

---
+++ Assets/_Project/Scripts/Ability/Ability.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Ability")]
+public class Ability : ScriptableObject {
+    public string Name;
+    public Sprite Icon;
+}
