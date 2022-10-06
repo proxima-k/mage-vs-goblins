946d604ddb7545c1920f973d5474a2faf507da58
proxima-k <leonnkent@gmail.com>
2022-10-07 00:51:35 +0800

    Implement a roughly working laser attack

--- Assets/_Project/Scripts/PlayerSkills.cs
+++ Assets/_Project/Scripts/PlayerSkills.cs
@@ -1,5 +1,6 @@
 using System;
 using Proxima_K.Utils;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
@@ -8,13 +9,19 @@ public class PlayerSkills : MonoBehaviour {
     [SerializeField] private Transform _fireballTf;
     [SerializeField] private float _fireballSpeed = 20f;
 
+    [Header("Laser Properties")]
+    [SerializeField] private float _laserDistance = 5f;
+    [SerializeField] private float _laserWidth = 2f;
+
     private void Start() {
         _cam = Camera.main;
     }
 
     private void Update() {
-        if (Input.GetMouseButtonDown(0))
-            ShootFireball();
+        if (Input.GetMouseButtonDown(0)) {
+            // ShootFireball();
+            ShootLaser();
+        }
     }
 
     private void ShootFireball() {
@@ -30,5 +37,26 @@ public class PlayerSkills : MonoBehaviour {
         // add velocity/force towards direction
         Vector2 targetDir = (PK.GetMouseWorldPosition2D(_cam) - (Vector2)transform.position).normalized;
         rb2D.velocity = targetDir * _fireballSpeed;
+    }
+
+    private void ShootLaser() {
+        // shoots a laser across a given distance
+        Vector2 targetDir = (PK.GetMouseWorldPosition2D(_cam) - (Vector2) transform.position).normalized;
+        float angle = Mathf.Atan2(targetDir.y, targetDir.y) * Mathf.Rad2Deg;
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(transform.position, new Vector2(0.5f,_laserWidth), angle, targetDir, _laserDistance);
+        foreach (var hit in hits) {
+            if(hit.transform.TryGetComponent(out IHealthDamageable healthDamageable))
+                healthDamageable.Damage(5);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Vector3 origin = transform.position;
+        Vector3 targetPos = PK.GetMouseWorldPosition2D(Camera.main);
+        Vector3 targetDir = (targetPos - origin).normalized;
+        Gizmos.DrawCube(origin, Vector3.one * _laserWidth);
+        Gizmos.DrawCube(origin+targetDir*_laserDistance, Vector3.one * _laserWidth);
+        Gizmos.DrawLine(origin, origin+targetDir*_laserDistance);
+        
     }
 }
