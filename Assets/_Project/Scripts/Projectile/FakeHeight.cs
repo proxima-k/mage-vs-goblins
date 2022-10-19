9c4db380b3a1e622f8d48637f4f85e6f93974846
proxima-k <leonnkent@gmail.com>
2022-10-19 22:10:51 +0800

    Add fireball prefab from previous project and add main menu scene

---
+++ Assets/_Project/Scripts/Projectile/FakeHeight.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class FakeHeight : Projectile {
+    // horizontal position
+    // vertical position (transform of object)
+    [SerializeField] private Transform _bodyTf;
+    [SerializeField] private float _gravity = 9.81f;
+
+    private float _currVelocity;
+    public event Action OnGroundHit;
+
+    private void Update() {
+        // update height value
+        _bodyTf.localPosition += new Vector3(0, _currVelocity * Time.deltaTime);
+
+        // checks if object hits the ground
+        CheckGroundHit();
+    }
+
+    public void Launch(float timeToReach, Vector3 targetPos) {
+        // initializes the starting vertical velocity
+        _currVelocity = _gravity * timeToReach/2;
+        
+        // move horizontally to target position within time
+        // Move((targetPos - transform.position)/timeToReach);
+    }
+
+    private void CheckGroundHit() {
+        // if the object hits the ground
+        if (_bodyTf.position.y < transform.position.y) {
+            _bodyTf.position = transform.position;
+            _currVelocity = 0;
+            OnGroundHit?.Invoke();
+        } 
+        // if the object is in air, then keep pulling it down
+        else if (_bodyTf.position.y > transform.position.y) {
+            _currVelocity -= _gravity * Time.deltaTime;
+        }
+    }
+}
